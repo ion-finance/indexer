d7a9fd3dc563d3d3c160fbce1acbf55c74f0bf12
devlbrian <brian@ionfi.xyz>
2023-08-03 19:57:53 +0900

    Fix error

--- src/tasks/fetchEvents.ts
+++ src/tasks/fetchEvents.ts
@@ -4,11 +4,10 @@ import { Event } from '../types/ton-api'
 
 const fetchEvents = async () => {
   const timestamp = await prisma.indexerState.getLastTimestamp()
+  const startTimeStamp = timestamp ? timestamp - 10 : 0
   const routerAddress = 'EQDEEwdjcctlXsOkNFeACn9z9wnZoUEoEyI_Pw0mnAM6FtS3'
   const res = await axios(
-    `${process.env.TON_API_URL}/accounts/${routerAddress}/events?start_date=${
-      timestamp - 10
-    }&limit=100`,
+    `${process.env.TON_API_URL}/accounts/${routerAddress}/events?start_date=${startTimeStamp}&limit=100`,
   )
 
   const events = res.data.events as Event[]
