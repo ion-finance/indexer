c47f86b8676367ada58803246c5ab968c23a14a5
alice-ion <alice@ionfi.xyz>
2023-08-08 14:15:58 +0900

    Optimize connections

--- src/clients/prisma/extensions/indexer.ts
+++ src/clients/prisma/extensions/indexer.ts
@@ -1,25 +1,26 @@
 import { Prisma } from '@prisma/client'
-import prisma from '..'
 
-const IndexerStateExtenstions = Prisma.defineExtension({
-  name: `IndexerStateExtensions`,
-  model: {
-    indexerState: {
-      async getLastTimestamp(): Promise<number> {
-        const lastTimestamp = await prisma.indexerState.findFirst({
-          where: { key: 'last_timestamp' },
-        })
-        return lastTimestamp ? Number(lastTimestamp.value) : 0
-      },
-      async setLastTimestamp(timestamp: number): Promise<void> {
-        await prisma.indexerState.upsert({
-          where: { key: 'last_timestamp' },
-          update: { value: timestamp.toString() },
-          create: { key: 'last_timestamp', value: timestamp.toString() },
-        })
+const IndexerStateExtenstions = Prisma.defineExtension((client) => {
+  return client.$extends({
+    name: `IndexerStateExtensions`,
+    model: {
+      indexerState: {
+        async getLastTimestamp(): Promise<number> {
+          const lastTimestamp = await client.indexerState.findFirst({
+            where: { key: 'last_timestamp' },
+          })
+          return lastTimestamp ? Number(lastTimestamp.value) : 0
+        },
+        async setLastTimestamp(timestamp: number): Promise<void> {
+          await client.indexerState.upsert({
+            where: { key: 'last_timestamp' },
+            update: { value: timestamp.toString() },
+            create: { key: 'last_timestamp', value: timestamp.toString() },
+          })
+        },
       },
     },
-  },
+  })
 })
 
 export default IndexerStateExtenstions
